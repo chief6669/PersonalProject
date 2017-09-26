bf0036b686a83add72fb48ca5433ba0d4f1c9d8b
chief6669 <sgareeb@g.hmc.edu>
2017-09-25 18:53:04 -0700

    Commit 2
    Adding servo driver code to view, should remove it later. Also adding changes to state estimation program. Added ability to plot. In for loop fixed rotation matrix update by using correct formula as well as normalizing the matrix to the determinant in each time step.

--- stateEstimationTest.py
+++ stateEstimationTest.py
@@ -1,19 +1,24 @@
 import math
 import numpy
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def main():
 	#First define time vector
-	time=list(range(0,11))
+	tInitial = 0
+	tFinal = 5
+	time=list(range(tInitial,tFinal))
 	#Generated my time vector now to generate local acceleration
+	gravity = 0
 	#Define anglular velcoity 
 	angularVelocity = [[0,0,0],[0,0,0]]
-	localAcceleration = [[0, 0, 0],[1,1,10.8]]
+	localAcceleration = [[0, 0, gravity],[1,1,gravity+1]]
 	#define intitial velocity
 	vg = [[0,0,0],[0,0,0]]
 	#define intial position
 	rg = [[0,0,0],[0,0,0]]
-	for i in range(2,11):
-		localAcceleration.append([0,0,9.8])
+	for i in range(2,len(time)):
+		localAcceleration.append([0,0,gravity])
 		angularVelocity.append([0,0,0])
 		vg.append([0,0,0])
 		rg.append([0,0,0])
@@ -34,8 +39,11 @@ def main():
 	rotationMatrix = numpy.dot(rollMatrix,pitchMatrix)
 	rotationMatrix = numpy.dot(rotationMatrix, yawMatrix)
 
+	identity=[[1, 0, 0],
+			  [0, 1, 0],
+			  [0, 0, 1]]
+
 	#Define gravity
-	g = 9.8
 	for t in range(1,len(time)):
 		w=angularVelocity[t]
 		al=localAcceleration[t]
@@ -44,14 +52,34 @@ def main():
 			   [w[2],0,-w[0]],
 			   [-w[1],w[0],0]]
 		#Update the rotation matrix
-		rotationMatrix=rotationMatrix+numpy.dot(rotationMatrix,omega*(time[t]-(time[t-1])))
+		omega=omega*(time[t]-time[t-1])
+		intermediate = numpy.add(omega,identity)
+		#print intermediate
+		rotationMatrix=numpy.dot(rotationMatrix,intermediate)
+		#Normalize Matrix
+		rotationMatrix=numpy.divide(rotationMatrix,numpy.cbrt(numpy.linalg.det(rotationMatrix)))
+		print(numpy.linalg.det(rotationMatrix))
 		#convert acceleration to global coordinates
 		ag=numpy.dot(rotationMatrix,al)
 		#Remove gravity term from ag
-		ag[2]=ag[2]-9.8
+		ag[2]=ag[2]-gravity
 		#integrate to get velocity
 		vg[t]=vg[t-1]+ag*(time[t]-time[t-1])
 		#integrate to get position
 		rg[t]=rg[t-1]+vg[t]*(time[t]-time[t-1])
 	#Next step is to figure out how to plot the position so that I can test variation in the code
+
+	#Starting Plot Section to help with Debugging of state estimation program
+	xData = []
+	yData = []
+	zData = []
+	for i in range(0,len(time)):
+		xData.append(rg[i][0])
+		yData.append(rg[i][1])
+		zData.append(rg[i][2])
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	ax.plot(xData,yData,zData)
+	plt.show()
+
 if __name__ == "__main__": main()
